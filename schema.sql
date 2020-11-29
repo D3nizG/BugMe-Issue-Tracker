7941e3feb2a760503c5fb79b8345e624f95798a9
Abygayle Ivey <abyivey@gmail.com>
2020-11-29 15:24:07 -0500

    insert users

--- schema.sql
+++ schema.sql
@@ -1,27 +1,33 @@
+DROP DATABASE IF EXISTS bugDB;
+CREATE DATABASE bugDB;
+USE bugDB;
 
-CREATE DATABASE bugme;
+
+CREATE TABLE `users` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`firstname` varchar(255) NOT NULL,
+	`lastname` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL UNIQUE,
+	`password` CHAR(32) NOT NULL,
+	`date_joined` DATETIME NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `issues` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`title` varchar(255) NOT NULL,
+	`description` TEXT,
+	`type` enum('bug','proposal','task') NOT NULL,
+	`priority` enum('minor','major','critical') NOT NULL,
+	`status` enum('open','closed','inprogress') NOT NULL,
+	`assigned_to` INT NOT NULL,
+	`created_by` INT NOT NULL,
+	`created` DATETIME NOT NULL,
+	`updated` DATETIME,
+	PRIMARY KEY (`id`)
+);
 
 
-CREATE TABLE 'users'(
-    'id' INT NOT NULL AUTO_INCREMENT,
-    'firstname' varchar(24) NOT NULL,
-    'lastname'varchar(24)NOT NULL,
-    'password' varchar(255) NOT NULL,
-    'email' varchar(255) NOT NULL UNIQUE,
-    'date_joined' DATETIME NOT NULL,
-    PRIMARY KEY ('id')
-)
 
-CREATE TABLE 'issues'(
-    'id' INT NOT NULL AUTO_INCREMENT,
-    'title' varchar(255) NOT NULL,
-    'description' TEXT,
-    'type' enum ('bug', 'proposal', 'task') NOT NULL,
-    'priority' enum ('Minor', 'Major','Critical') NOT NULL UNIQUE,
-    'status' varchar (25),
-    'assigned_to' INT NOT NULL,
-    'created_by' INT NOT NULL,
-    'created' DATETIME NOT NULL,
-    'updated' DATETIME,
-    PRIMARY KEY ('id')
-)
+INSERT into users(firstname,lastname,password,email,date_joined)
+VALUES ("Tom","Bill",SHA2('password123',512),"admin@project2.com",NOW());
