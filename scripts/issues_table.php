fea993e743238c87d84fc37791674f2c82a2248b
Javian Anderson <javian99anderson@gmail.com>
2020-12-03 05:02:37 -0500

    Fixed Table. Link to Descriptions left

--- scripts/issues_table.php
+++ scripts/issues_table.php
@@ -5,12 +5,8 @@ require_once 'dbconfig.php';
 try{
     $button=$_POST['button'];
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password1);
-    // $stmt = $conn->query("SELECT c.title,c.descrip,c.typeof,c.stat,c.assigned_to,c.created FROM issues c INNER JOIN users c1 on c.assigned_to = c1.id");
     $stmt = $conn->query("SELECT c.title,c.descrip,c.typeof,c.stat,c.assigned_to,c.created, c1.firstname, c1.lastname, c1.id  FROM issues c, users c1 WHERE c.assigned_to=c1.id");
     $results = $stmt ->fetchALL(PDO ::FETCH_ASSOC);
-    // $stmt = ("SELECT c.title,c.descrip,c.typeof,c.stat,c.assigned_to,c.created FROM issues c LEFT JOIN user c1 on c.assigned_to = c1.id");
-    // $results = $stmt ->fetchALL(PDO ::FETCH_ASSOC);
-    // $results = mysqli_query($conn,"SELECT c.title,c.descrip,c.typeof,c.stat,c.assigned_to,c.created FROM issues c LEFT JOIN user c1 on c.assigned_to = c1.id");
     if($button=='all'){
         echo '<table>';
         echo '<tr>';
@@ -22,17 +18,18 @@ try{
         echo '<th>Created</th>';
         echo '</tr>';
         foreach($results as $row){
+            $datetime = new DateTime($row['created']);
+            $date1 = $datetime->format('Y-m-d');
             echo '<tr>';
             echo '<td>'.$row['title'].'</td>';
             echo '<td>'.$row['descrip'].'</td>';
-            echo '<td>'.$row['typeof'].'</td>';
-            echo '<td>'.$row['stat'].'</td>';
+            echo '<td class="typeof">'.$row['typeof'].'</td>';
+            echo '<td   class="status">'.$row['stat'].'</td>';
             echo '<td>'.$row['firstname'].' '.$row['lastname'].'</td>';
-            echo '<td>'.$row['created'].'</td>';
+            echo '<td>'.$date1.'</td>';
             echo '</tr>';
         }
         echo '</table>';
-        // print_r($results);
     }
     elseif($button=='open'){
         echo '<table>';
@@ -46,13 +43,15 @@ try{
         echo '</tr>';
         foreach($results as $row){
             if($row['stat']=="open"){
+                $datetime = new DateTime($row['created']);
+                $date1 = $datetime->format('Y-m-d');
                 echo '<tr>';
                 echo '<td>'.$row['title'].'</td>';
                 echo '<td>'.$row['descrip'].'</td>';
-                echo '<td>'.$row['typeof'].'</td>';
-                echo '<td>'.$row['stat'].'</td>';
+                echo '<td class="typeof">'.$row['typeof'].'</td>';
+                echo '<td   class="status">'.$row['stat'].'</td>';
                 echo '<td>'.$row['firstname'].' '.$row['lastname'].'</td>';
-                echo '<td>'.$row['created'].'</td>';
+                echo '<td>'.$date1.'</td>';
                 echo '</tr>';
             }
         }
@@ -69,14 +68,16 @@ try{
         echo '<th>Created</th>';
         echo '</tr>';
         foreach($results as $row){
+            $datetime = new DateTime($row['created']);
+            $date1 = $datetime->format('Y-m-d');
             if($row['id']=="1"){
                 echo '<tr>';
                 echo '<td>'.$row['title'].'</td>';
                 echo '<td>'.$row['descrip'].'</td>';
-                echo '<td>'.$row['typeof'].'</td>';
-                echo '<td>'.$row['stat'].'</td>';
+                echo '<td class="typeof">'.$row['typeof'].'</td>';
+                echo '<td  class="status">'.$row['stat'].'</td>';
                 echo '<td>'.$row['firstname'].' '.$row['lastname'].'</td>';
-                echo '<td>'.$row['created'].'</td>';
+                echo '<td>'.$date1.'</td>';
                 echo '</tr>';
             }
         }
--- scripts/table.js
+++ scripts/table.js
@@ -11,11 +11,6 @@ function button() {
         data:{"button":"all"},
         success: function(data){
             $("#result").html(data);
-            // htmll.insertAdjacentHTML('beforeend',data);
-            // const tempWrapper = document.createElement('div');
-            // tempWrapper.innerHTML = data;
-            // document.getElementById('result').appendChild(tempWrapper.firstChild);
-            console.log(data);
             }
         }); 
     });
@@ -28,11 +23,6 @@ function button() {
          data:{"button":"open"},
          success: function(data){
              $("#result").html(data);
-             // htmll.insertAdjacentHTML('beforeend',data);
-             // const tempWrapper = document.createElement('div');
-             // tempWrapper.innerHTML = data;
-             // document.getElementById('result').appendChild(tempWrapper.firstChild);
-             console.log(data);
              }
          }); 
      });
@@ -44,15 +34,45 @@ function button() {
          data:{"button":"mytickets"},
          success: function(data){
              $("#result").html(data);
-             // htmll.insertAdjacentHTML('beforeend',data);
-             // const tempWrapper = document.createElement('div');
-             // tempWrapper.innerHTML = data;
-             // document.getElementById('result').appendChild(tempWrapper.firstChild);
-             console.log(data);
              }
          }); 
      });
     
 }
 
+
 window.onload=button;
+
+window.setInterval(function(){
+    var ths = document.getElementsByClassName('status');
+    var tds = document.getElementsByClassName('typeof');
+    for(let a of ths){
+        var textt= a.innerHTML;
+        if(textt=='open'){
+            a.innerHTML="OPEN";
+            a.style.backgroundColor='green';
+            a.style.color='white';
+        }
+        else if(textt=='closed'){
+            a.innerHTML="CLOSED";
+            a.style.backgroundColor='orange';
+            a.style.color='white';
+        }
+        if(textt=='in progress'){
+            a.innerHTML="IN PROGRESS";
+            a.style.backgroundColor='rgb(255,255,0)';
+        }
+    }
+    for(let b of tds){
+        var text1 = b.innerHTML;
+        if(text1=='bug'){
+            b.innerHTML="Bug";
+        }
+        else if(text1=='proposal'){
+            b.innerHTML="Proposal";
+        }
+        else if(text1=='task'){
+            b.innerHTML="Task";
+        }
+    }
+  }, 100);
