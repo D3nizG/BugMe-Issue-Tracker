5ba472be81f3febb04f78e46954d23c545695cc1
Deandrew007 <deamoore07@gmail.com>
2020-12-03 08:53:03 -0500

    commit before pulling javian changes

--- scripts/login.php
+++ scripts/login.php
@@ -1,34 +1,39 @@
-<?php
-
+<?php 
+session_start();
 require_once 'dbconfig.php';
+?>
 
-try {
-    $email = $_POST['email'];
-    $sanitizedEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
-    $password = $_POST['psw'];
-    $sanitizedPassword = filter_var($password, FILTER_SANITIZE_STRING	);
+<?php 
 
-      //email validation
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $emailErr = "Invalid email format";
-      }
+$email = $_POST['email'];
+$sanitizedEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+$password = $_POST['psw'];
+$sanitizedPassword = filter_var($password, FILTER_SANITIZE_STRING   );
+$errorMsg = '';
 
 
-    if(password_verify($sanitizedPassword, $hashed_password)) {
-        
-    } 
+if(isset($_POST["login"])) {
+    if ($email != '' && $password != ''){
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password1);
+            echo "Connected to $dbname at $host";
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+            $sql = $conn->query("SELECT email, pword, firstname FROM users");
+            $results = $sql ->fetchALL(PDO ::FETCH_ASSOC);
 
+            if($results == 1 && password_verify($sanitizedPassword, $row['pword'])) {
+                $_SESSION['sessionEmail'] = $row['email'];
+                $_SESSION['sessionUserName'] = $row['firstname'];
 
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password1);
-    echo "Connected to $dbname at $host";
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT into users(firstname,lastname,pword,email, NOW())
-    VALUES ('$firstname','$lastname','$hashed_password','$email')";
-    $conn->exec($sql);
+            }else{
+                $errorMsg = '<label>Invalid Credentials, try again.</label>';
+        }
+        } catch (PDOException $pe) {
+            die("Could not connect to the database $dbname :" . $pe->getMessage());
+        }
+    }
 
-} catch (PDOException $pe) {
-    die("Could not connect to the database $dbname :" . $pe->getMessage());
 }
 $conn = null;
 ?>
